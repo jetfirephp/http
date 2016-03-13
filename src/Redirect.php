eee5a196f4d954aabf81923ee79fffb0380f0bcc
Mugan27 <sumu94@hotmail.fr>
2016-03-13 18:43:01 +0100

    remove with method in redirect

--- src/Redirect.php
+++ src/Redirect.php
@@ -6,17 +6,6 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class Redirect extends RedirectResponse{
 
-    protected $session;
-    protected $request;
-
-    public function setSession(Session $session){
-        $this->session = $session;
-    }
-
-    public function setRequest(Request $request){
-        $this->request = $request;
-    }
-
     public function to($url,$status = 302,$type = 'text/html'){
         if(!is_null($status))$this->setStatusCode($status);
         $this->headers->set('Content-type',$type);
@@ -26,14 +15,6 @@ class Redirect extends RedirectResponse{
 
     public function back(){
         $this->setTargetUrl($this->request->server['HTTP_REFERER']);
-        return $this;
-    }
-
-    public function with($key, $value = null)
-    {
-        $key = is_array($key) ? $key : [$key => $value];
-        foreach ($key as $k => $v)
-            $this->session->flash($k, $v);
         return $this;
     }
 
